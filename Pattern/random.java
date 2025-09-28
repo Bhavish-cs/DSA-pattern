eb391aa9b34785748bffd2dc10f5ef6562c1755d
Bhavish-cs <bhavish.gowda2k4@gmail.com>
2025-09-29 04:44:30 +0530

    new changes

--- Pattern/random.java
+++ Pattern/random.java
@@ -10,7 +10,12 @@ public class random {
         int n = sc.nextInt();
 
         System.out.println("OutPut:\n");
-       armsstrongnumber(n);
+        primenumber(n);
+        if (primenumber(n)) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
     }
     //number of the digit in the one number 
 
@@ -51,7 +56,7 @@ public class random {
         int p = String.valueOf(n).length();
         while (temp > 0) {
             int digit = temp % 10;
-            sum += Math.pow(digit,p);
+            sum += Math.pow(digit, p);
             temp = temp / 10;
         }
         if (num == sum) {
@@ -60,6 +65,36 @@ public class random {
             System.out.println("Not an armstrong");
         }
 
+    }
+    //division rule 
+
+    public static void divisionrule(int n) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+                if (i != n / i) {
+                    System.out.println(n / i);
+                }
+            }
+        }
+    }
+
+    public static boolean  primenumber(int n) {
         
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        int limit = (int) Math.sqrt(n);
+        for (int i = 3; i <= limit; i +=2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+
+
     }
+    
+
     }
+
+    
