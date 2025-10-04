657fa8c5c1a9df96421953a43e9c210fcd725a23
Bhavish-cs <bhavish.gowda2k4@gmail.com>
2025-10-04 19:29:04 +0530

    some dsa about the  recurssion concept

--- Pattern/recurssion.java
+++ Pattern/recurssion.java
@@ -3,29 +3,54 @@ import java.util.Scanner;
 public class recurssion {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number:");
+
+        System.out.print("Enter the number of Fibonacci elements: ");
         int n = sc.nextInt();
 
-        int sum = recurssion2( n);  // fixed: provide both parameters
-        System.out.println("Sum = " + sum);
+        System.out.println("Fibonacci series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(recursion4(i) + " ");
+        }
+        System.out.println();
 
+        sc.close();
     }
 
-    public static int recurssion1(int i, int n) {
+    // Sum from i to n
+    public static int recursion1(int i, int n) {
         if (i > n) {
             return 0;
         } else {
-            return i + recurssion1(i + 1, n);
+            return i + recursion1(i + 1, n);
         }
     }
 
-    public static int recurssion2(int n) {
+    // Sum from n to 1
+    public static int recursion2(int n) {
         if (n == 0) {
             return 0;
         } else {
-            return n + recurssion2(n - 1);
+            return n + recursion2(n - 1);
         }
+    }
+
+    // Reverse array recursively
+    public static void recursion3(int i, int j, int arr[]) {
+        if (i >= j) {
+            return;
+        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        recursion3(i + 1, j - 1, arr);
+    }
+
+    // Fibonacci using recursion
+    public static int recursion4(int n) {
+        if (n <= 1) {
+            return n; // Base cases: Fib(0)=0, Fib(1)=1
+        }
+        return recursion4(n - 1) + recursion4(n - 2);
     }
 }
